a849b1a755f27b21425b2bdd3d5a040d218187cd
陳奕安 <kk@chenyiandeMacBook-Air-2.local>
2026-05-05 10:13:56 +0800

    feat: assemble complete homepage with all sections

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,19 @@
+import HeroBanner from '@/components/home/HeroBanner'
+import StatsBar from '@/components/home/StatsBar'
+import ProductsPreview from '@/components/home/ProductsPreview'
+import ToyExchangeHighlight from '@/components/home/ToyExchangeHighlight'
+import GreenCoinHighlight from '@/components/home/GreenCoinHighlight'
+import NewsPreview from '@/components/home/NewsPreview'
+
 export default function HomePage() {
-  return <div>Home page - coming soon</div>
+  return (
+    <>
+      <HeroBanner />
+      <StatsBar />
+      <ProductsPreview />
+      <ToyExchangeHighlight />
+      <GreenCoinHighlight />
+      <NewsPreview />
+    </>
+  )
 }
